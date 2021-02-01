96601ecca362913b9ef94123cdce8065cb727658
kosiazom <kosiazom@gmail.com>
2021-02-01 13:36:37 -0500

    adding alert and timer

--- setup/src/App.js
+++ setup/src/App.js
@@ -7,30 +7,62 @@ function App() {
   const [list, setList] = useState([]);
   const [isEditing, stIsEditing] = useState(false);
   const [editID, setEditId] = useState(null);
-  const [alert, setAlert] = useState({show: false, msg: '', type: ''})
+  const [alert, setAlert] = useState({
+      show: false,
+      msg: '', 
+      type: ''})
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("i was hit!")
+   if(!name) {
+     // display the alert here! change the state here
+    //  setAlert({
+    //    show:true,
+    //    msg: 'please enter value!',
+    //    type: "danger"
+    //  })
+    showAlert(true, "danger", "please enter value")
+
+   } else if( name && isEditing ){
+
+   } //if there is something in the value and isEditting is true then display alerts
+   else {
+     //show alert
+     const newItem = {id: new Date(). getTime().toString(), title: name};
+     setList([...list, newItem]);
+     setName('') // this clears the form
+   }
+  }
+// this function below is ES6 and th arguments can take p
+  const showAlert = (show=false, type="", msg="") => {
+    setAlert({ show, type, msg })
+
   }
 
   return <section className="section-center">
     <form className="grocery-form" onSubmit={handleSubmit}>
-      {alert.show && <Alert />}
+      {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
       <h3>grocery bud</h3>
       <div className='form-control'>
-        <input type="text" className='grocery' placeholder="e.g. bananas"/>
+        <input type="text" 
+               className='grocery' 
+               placeholder="e.g. bananas"
+               value={name}
+               onChange={ (e) => setName(e.target.value) } />
         <button type="submit" className='submit-btn'>
           {isEditing ? "edit"  : "Submit"}
         </button>
       </div>
     </form>
+    {list.length > 0 && (
+
     <div className="grocery-container">
-      <List />
+      <List  items={list}/>
       <button className="clear-btn">
         clear items
       </button>
     </div>
+    )}
   </section>
 }
 
--- setup/src/List.js
+++ setup/src/List.js
@@ -1,7 +1,23 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+const List = ({items}) => {
+  console.log(items)
+  return <div className="grocery-list">
+    {items.map((item) => {
+      const {id, title} = item; //destructured immediately
+      return <article key={id} className="grocery-item">
+        <p className="title">{title}</p>
+        <div className="btn-container">
+          <button type="button" className="edit-btn">
+            <FaEdit />
+          </button>
+          <button type="button" className="delete-btn">
+            <FaTrash />
+          </button>
+        </div>
+      </article>
+    })}
+  </div>
 }
 
 export default List
